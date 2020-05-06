856be2e982f4e472539290e083039716b5e089af
Madison Miatke <miatkem@gmail.com>
2020-05-06 00:20:06 -0400

    Add files via upload

---
+++ ImageLoader.java
@@ -0,0 +1,24 @@
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageLoader
+{
+
+	/*LOADS IMAGE INTO BUFFERED IMAGE*/
+	/*checks for errors in file reading*/
+	public static BufferedImage loadImage(String path)
+	{
+		try {
+			return ImageIO.read(ImageLoader.class.getResource(path));
+		}catch (IOException e) {
+
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		return null;
+	}
+
+}
